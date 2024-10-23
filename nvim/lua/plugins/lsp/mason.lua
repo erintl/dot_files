ce115f71bf7340137ef837a1e6ba2f3de305b743
Tim Erin <erintl@gmail.com>
2024-10-22 23:48:51 -0400

    Adding noice.nvim

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -24,10 +24,9 @@ return {
       mason_lspconfig.setup({
         -- list of servers for mason to install
         ensure_installed = {
-          "tsserver",
+          "ts_ls",
           "html",
           "cssls",
-          -- "tailwindcss",
           "lua_ls",
           "graphql",
           "emmet_ls",
